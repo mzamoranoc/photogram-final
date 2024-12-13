b52d8d0e4668eacaf467d41776a8564ba278bded
mzamoranoc <mzamoran@chicagobooth.edu>
2024-12-13 07:36:41 +0000

    fixing likes missing

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,8 @@ class User < ApplicationRecord
   has_many :photos, foreign_key: :owner_id, dependent: :destroy
   has_many :comments, foreign_key: :author_id, dependent: :destroy
   has_many :likes, foreign_key: :fan_id, dependent: :destroy
-
+  has_many :liked_photos, through: :likes, source: :photo 
+  
   # Follow Requests
   has_many :sent_follow_requests, class_name: 'FollowRequest', foreign_key: :sender_id, dependent: :destroy
   has_many :received_follow_requests, class_name: 'FollowRequest', foreign_key: :recipient_id, dependent: :destroy
