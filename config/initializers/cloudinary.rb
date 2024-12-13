2db2c35846a39a771c73c3801487610f1bf181da
mzamoranoc <mzamoran@chicagobooth.edu>
2024-12-13 07:22:03 +0000

    updating the show erb for the user

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,13 @@
+#Cloudinary.config do |config|
+#  config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
+#  config.api_key    = ENV['CLOUDINARY_API_KEY']
+#  config.api_secret = ENV['CLOUDINARY_API_SECRET']
+#  config.cdn_subdomain = true
+#end
+
 Cloudinary.config do |config|
-  config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
-  config.api_key    = ENV['CLOUDINARY_API_KEY']
-  config.api_secret = ENV['CLOUDINARY_API_SECRET']
-  config.cdn_subdomain = true
+  config.cloud_name = 'dya3llufu'
+  config.api_key = '112841734841699'
+  config.api_secret = 'fg_6atGVkJbAL7xOurkc86a5TmQ'
+  config.secure = true
 end
